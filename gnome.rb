03b310353cba8dc405f10b07a823f7e9f1ec857f
oliviakris24 <okfishy@gmail.com>
2018-07-13 16:23:19 +0000

    automatically backed up by learn

---
+++ gnome.rb
@@ -0,0 +1,52 @@
+# Code your instances here
+class GardenGnome
+  
+  attr_accessor :gluten_allergy
+
+def initialize(personality, hat_color, gnome1)
+  @personality = personality
+  @hat_color = hat_color
+  @gnome1 = gnome1 
+  @gnome2 = gnome2
+  @gnome3 = gnome3
+end 
+  
+def personality
+  @personality = evil
+end 
+  
+def hat_color = hat_color.new  
+  @hat_color 
+end 
+
+def gnome1 
+  @gnome1 = "Walter the Worst"
+end 
+
+def gnome2
+  @gnome2 = "James the Jerk"
+end 
+
+def gnome3 
+  
+  
+def age
+  @age
+end 
+
+def age = (age)
+  @age = age
+end 
+
+def gnaw
+  return "Gnawing on a tree"
+end 
+  
+def shout 
+  return "GNARLY"
+end 
+  
+def introduce_self
+    return "Hello humans, my name is #{introduce_self}, I am #{age} years old, and you'll rue the day you crossed me!"
+end 
+end   
