a4ca423db3215062c038ee668304ee8b4f773276
oliviakris24 <okfishy@gmail.com>
2018-07-13 16:10:13 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,29 +1,32 @@
 # Code your instances here
 class GardenGnome
+  
   attr_accessor :gluten_allergy
-  def initialize(personality, hat_color)
-    @personality = personality
-    @hat_color = hat_color
-  end 
+
+def initialize(personality, hat_color)
+  @personality = personality
+  @hat_color = hat_color
+end 
   
-  def personality
-    @personality = evil
-  end 
+def personality
+  @personality = evil
+end 
   
-  def hat_color 
-    @hat_color = 
-  end 
+def hat_color = hat_color.new  
+  @hat_color 
+end 
   
-  def gnaw
-    return "Gnawing on a tree"
-  end 
+def gnaw
+  return "Gnawing on a tree"
+end 
   
-  def shout 
-    return "GNARLY"
-  end 
+def shout 
+  return "GNARLY"
+end 
   
-  def introduce_self
-    return "Hello humans, my name is, I am years old, and you'll rue the day you crossed me!"
-  end 
+def introduce_self
+    return "Hello humans, my name is #{introduce_self}, I am #{age} years old, and you'll rue the day you crossed me!"
+end 
+end 
     
     
